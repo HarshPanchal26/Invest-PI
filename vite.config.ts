35a202a9dad638bd789ed05382fc77c99ba6c27e
unknown <hrppanchal27@gmail.com>
2023-11-08 23:48:26 +0530

    Update Config File for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     },
     build: {
         lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
+            entry: resolve(__dirname, 'src/index.tsx'),
             name: 'myLib',
             fileName: 'myLib',
         },
