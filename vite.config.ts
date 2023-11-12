5e1aa8db4d3d7b338b4506fe625115ff1d8c8dda
unknown <hrppanchal27@gmail.com>
2023-11-12 19:47:50 +0530

    Added new Vite Variable with base

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig} from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import envCompatible from 'vite-plugin-env-compatible'
@@ -9,9 +9,7 @@ dns.setDefaultResultOrder('verbatim')
 
 export default defineConfig(({mode}) => {
     // Load environment variables
-    const env = loadEnv(mode, process.cwd(), '');
     return {
-        base: '/Invest-PI/', 
         define: {
             // 'process.env': env,
             'process.env.NODE_ENV': JSON.stringify(mode),
