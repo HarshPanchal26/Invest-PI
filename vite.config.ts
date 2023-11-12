55be97ed694f86396b22872eb58160bc1679a1f7
unknown <hrppanchal27@gmail.com>
2023-11-12 16:03:48 +0530

    Change Vite file again 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,52 @@
-import { defineConfig , loadEnv } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import viteTsconfigPaths from 'vite-tsconfig-paths'
 import dns from 'dns'
 dns.setDefaultResultOrder('verbatim')
- 
-// Load environment variables
-const env = loadEnv('production', process.cwd(), '');
 
-export default defineConfig({
-    // base: '/Invest-PI/',
-    define: {
-        'process.env': env,
-    },
-    plugins: [react(), viteTsconfigPaths()],
-    css: {
-        postcss: {
-            plugins: [require('tailwindcss'), require('autoprefixer')],
+
+export default defineConfig(({mode}) => {
+    // Load environment variables
+    const env = loadEnv(mode, process.cwd(), '');
+    
+    return {
+        // base: '/Invest-PI/',
+        define: {
+            // 'process.env': import.meta.env,
+            'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
         },
-    },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:5000', // Set the target to yosur server
-                changeOrigin: true, // Change the origin to match the target
-                rewrite: (path) => path.replace(/^\/api/, ''), // Optionally rewrite the path
+        plugins: [react(), viteTsconfigPaths()],
+        css: {
+            postcss: {
+                plugins: [require('tailwindcss'), require('autoprefixer')],
             },
         },
-        // Add this line to handle client-side routing
-        open: true,
-        host: "localhost",
-        port: 8080
-    },
-    build: {
-        lib: {
-            entry: resolve(__dirname, '/index.html'),
-            name: 'myLib',
-            fileName: 'myLib',
+        server: {
+            proxy: {
+                '/api': {
+                    target: 'http://localhost:5000', // Set the target to yosur server
+                    changeOrigin: true, // Change the origin to match the target
+                    rewrite: (path) => path.replace(/^\/api/, ''), // Optionally rewrite the path
+                },
+            },
+            // Add this line to handle client-side routing
+            open: true,
+            host: "localhost",
+            port: 8080
         },
-        rollupOptions: {
-            external: [/^node:\w+/], // <-- ignores all 'node:*'
+        build: {
+            lib: {
+                entry: resolve(__dirname, '/index.html'),
+                name: 'myLib',
+                fileName: 'myLib',
+            },
+            rollupOptions: {
+                external: [/^node:\w+/], // <-- ignores all 'node:*'
+            },
         },
-    },
+    }
 
 })
+
+
