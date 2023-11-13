d660a091f800d2cc55f96d1b89144cff0d4358bd
unknown <hrppanchal27@gmail.com>
2023-11-13 17:16:26 +0530

    Added Vite file

--- src/components/Authentication/LogInPage.tsx
+++ src/components/Authentication/LogInPage.tsx
@@ -55,7 +55,7 @@ export default function LogInPage() {
       const res: any = await verifyDataForLogIn(loginData);
       if (res?.Verified) { 
         setLoaderForLogin(true);
-        axios.post('https://api.investipi.com/login/user', loginData)
+        axios.post(`${process.env.VITE_APP_API_URL}/login/user`, loginData)
           .then((res) => {
             window.location.href = '/feed'
           }).catch((error) => {
