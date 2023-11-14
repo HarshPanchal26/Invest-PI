5050be892a464f62b5bcf56246794e1d3fd9ff3b
unknown <hrppanchal27@gmail.com>
2023-11-14 21:58:55 +0530

    Change the logo

--- src/Assets/logo.tsx
+++ src/Assets/logo.tsx
@@ -1,11 +1,10 @@
-import PILogo from '../../public/WhatsApp Image 2023-11-08 at 14.10.56.jpeg'
+import PILogo from '../../public/officialInvestiPI.png'
 
 export default function Logo() {
     return (
         <img
             className="mx-auto h-10 w-auto"
-            src="https://user-images.strikinglycdn.com/res/hrscywv4p/image/upload/c_limit,h_630,w_1200/833115/new-Logo-circle_tlzqsx.gif"
-            // src={PILogo}
+            src={PILogo}
             alt="Your Company"
         />
     )
