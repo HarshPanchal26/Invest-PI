327bb656a431bce3cfa61e1c77433409536513dd
unknown <hrppanchal27@gmail.com>
2023-11-17 21:42:26 +0530

    Reverse Changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,22 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { useNavigate } from 'react-router-dom';
 import IndexForRoute from './components/Route/index.route'
-import Loading from './Assets/Loading';
+// import Loading from './Assets/Loading';
 
 function App() {
 
-  const [loader, setLoader] = useState<boolean>(true);
-  const navigate = useNavigate();
+  // const [loader, setLoader] = useState<boolean>(true);
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    if (window.location.protocol === 'http:') {
-      const httpsURL = 'https://' + window.location.host + window.location.pathname;
-      // navigate(httpsURL, { replace: true });
-      console.log("Yes is is http")
-    }else{
-      setLoader(false)
-    }
-  }, [navigate]);
+  // useEffect(() => {
+  //   if (window.location.protocol === 'http:') {
+  //     const httpsURL = 'https://' + window.location.host + window.location.pathname;
+  //     // navigate(httpsURL, { replace: true });
+  //     console.log("Yes is is http")
+  //   }else{
+  //     setLoader(false)
+  //   }
+  // }, [navigate]);
 
   return (
     <>
